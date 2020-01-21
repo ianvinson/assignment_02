ac062ebd2e3d5b2d43e6b04bb54200515d56bb5a
ianvinson <60161085+ianvinson@users.noreply.github.com>
2020-01-21 16:13:10 -0700

    Add files via upload

---
+++ assign02/TestShit.java
@@ -0,0 +1,19 @@
+package assign02;
+
+import java.util.GregorianCalendar;
+
+public class TestShit
+{
+    private static GregorianCalendar dueDate;
+    
+    private static Object o;
+    
+    public static void main (String[] args)
+    {
+        dueDate = new GregorianCalendar(2020, 0, 0, 0, 0, 0);
+        o = dueDate;
+        
+
+    }
+
+}
