4fee5ab9b4e9688324afb657e7d367258bb116ff
Shengke Scott Xu <u1264557@utah.edu>
2020-01-22 16:26:40 -0700

    Update LibraryTester.java

--- assign02/LibraryTester.java
+++ assign02/LibraryTester.java
@@ -5,8 +5,6 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
