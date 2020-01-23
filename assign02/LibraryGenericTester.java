58fadf4a4985f57159f11b609aef0eaab53a03ad
Shengke Scott Xu <u1264557@utah.edu>
2020-01-22 17:34:24 -0700

    Update LibraryGenericTester.java

--- assign02/LibraryGenericTester.java
+++ assign02/LibraryGenericTester.java
@@ -14,19 +14,19 @@ import java.util.ArrayList;
  * @version January 16, 2020
  */
 public class LibraryGenericTester {
-	
-	private LibraryGeneric<String> nameLib;  // library that uses names to identify patrons (holders)
-	private LibraryGeneric<PhoneNumber> phoneLib;  // library that uses phone numbers to identify patrons
+
+	private LibraryGeneric<String> nameLib; // library that uses names to identify patrons (holders)
+	private LibraryGeneric<PhoneNumber> phoneLib; // library that uses phone numbers to identify patrons
 	LibraryBookGeneric<String> book1, book2, book3;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 		book1 = new LibraryBookGeneric<String>(9780374292799L, "Thomas L. Friedman", "The World is Flat");
 		book2 = new LibraryBookGeneric<String>(9780330351690L, "Jon Krakauer", "Into the Wild");
 		book3 = new LibraryBookGeneric<String>(9780446580342L, "David Baldacci", "Simple Genius");
-		
+
 		nameLib = new LibraryGeneric<String>();
-		
+
 		nameLib.add(9780374292799L, "Thomas L. Friedman", "The World is Flat");
 		nameLib.add(9780330351690L, "Jon Krakauer", "Into the Wild");
 		nameLib.add(9780446580342L, "David Baldacci", "Simple Genius");
@@ -34,9 +34,9 @@ public class LibraryGenericTester {
 		phoneLib = new LibraryGeneric<PhoneNumber>();
 		phoneLib.add(9780374292799L, "Thomas L. Friedman", "The World is Flat");
 		phoneLib.add(9780330351690L, "Jon Krakauer", "Into the Wild");
-		phoneLib.add(9780446580342L, "David Baldacci", "Simple Genius");		
+		phoneLib.add(9780446580342L, "David Baldacci", "Simple Genius");
 	}
-	
+
 	@Test
 	public void testNameLibCheckout() {
 		String patron = "Jane Doe";
@@ -50,7 +50,7 @@ public class LibraryGenericTester {
 		nameLib.checkout(9780330351690L, patron, 1, 1, 2008);
 		nameLib.checkout(9780374292799L, patron, 1, 1, 2008);
 		ArrayList<LibraryBookGeneric<String>> booksCheckedOut = nameLib.lookup(patron);
-		
+
 		assertNotNull(booksCheckedOut);
 		assertEquals(2, booksCheckedOut.size());
 		assertTrue(booksCheckedOut.contains(new Book(9780330351690L, "Jon Krakauer", "Into the Wild")));
@@ -58,7 +58,7 @@ public class LibraryGenericTester {
 		assertEquals(patron, booksCheckedOut.get(0).getHolder());
 		assertEquals(patron, booksCheckedOut.get(1).getHolder());
 	}
-	
+
 	@Test
 	public void testNameLibCheckin() {
 		String patron = "Jane Doe";
@@ -80,7 +80,7 @@ public class LibraryGenericTester {
 		phoneLib.checkout(9780330351690L, patron, 1, 1, 2008);
 		phoneLib.checkout(9780374292799L, patron, 1, 1, 2008);
 		ArrayList<LibraryBookGeneric<PhoneNumber>> booksCheckedOut = phoneLib.lookup(patron);
-		
+
 		assertNotNull(booksCheckedOut);
 		assertEquals(2, booksCheckedOut.size());
 		assertTrue(booksCheckedOut.contains(new Book(9780330351690L, "Jon Krakauer", "Into the Wild")));
@@ -96,43 +96,43 @@ public class LibraryGenericTester {
 		phoneLib.checkout(9780374292799L, patron, 1, 1, 2008);
 		assertTrue(phoneLib.checkin(patron));
 	}
-	
+
 	@Test
 	public void testGetInventoryList() {
 		ArrayList<LibraryBookGeneric<String>> expectedInventoryList = new ArrayList<LibraryBookGeneric<String>>();
 		expectedInventoryList.add(book2);
 		expectedInventoryList.add(book1);
-		expectedInventoryList.add(book2);
-		
+		expectedInventoryList.add(book3);
+
 		ArrayList<LibraryBookGeneric<String>> books = nameLib.getOrderedByTitle();
-		assertEquals(expectedInventoryList,books);
+		assertEquals(expectedInventoryList, books);
 	}
-	
+
 	@Test
 	public void testGetOverdueList() {
 		ArrayList<LibraryBookGeneric<String>> expectedOverDueList = new ArrayList<LibraryBookGeneric<String>>();
 		expectedOverDueList.add(book3);
 		expectedOverDueList.add(book1);
 		expectedOverDueList.add(book2);
-		
+
 		String patron = "Jane Doe";
 		nameLib.checkout(9780374292799L, patron, 1, 5, 2008);
 		nameLib.checkout(9780330351690L, patron, 2, 1, 2008);
 		nameLib.checkout(9780446580342L, patron, 12, 1, 2007);
 
 		ArrayList<LibraryBookGeneric<String>> overDueList = nameLib.getOverdueList(6, 1, 2008);
-		
-		assertEquals(expectedOverDueList,overDueList);
+
+		assertEquals(expectedOverDueList, overDueList);
 	}
-	
+
 	@Test
 	public void testGetOrderedByTitle() {
 		ArrayList<LibraryBookGeneric<String>> expectedOrderedByTitleList = new ArrayList<LibraryBookGeneric<String>>();
 		expectedOrderedByTitleList.add(book2);
 		expectedOrderedByTitleList.add(book3);
 		expectedOrderedByTitleList.add(book1);
-		
+
 		ArrayList<LibraryBookGeneric<String>> books = nameLib.getOrderedByTitle();
-		assertEquals(expectedOrderedByTitleList,books);
+		assertEquals(expectedOrderedByTitleList, books);
 	}
 }
